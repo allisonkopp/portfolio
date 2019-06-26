b70936cbaff29f8bf0ee94ef9e6d9bdf1c25eb44
Allison Kopp <allison.kopp03@gmail.com>
2019-06-25 20:15:32 -0400

    added buttons

--- client/src/components/Heading/index.js
+++ client/src/components/Heading/index.js
@@ -1,70 +1,28 @@
 import React from 'react';
 import anime from 'animejs';
-import './Heading.css';
+import './Heading.scss';
 import $ from 'jquery';
 
 import WORKSPACE from '../../assets/images/workspace.jpg';
 
 const heading = 'SOFTWARE ENGINEER';
 
-const Heading = props => {
-  // $('.ml9 .letters').each(function() {
-  //   $(this).html(
-  //     $(this)
-  //       .text()
-  //       .replace(/([^\x00-\x80]|\w)/g, <span class="letter">$&</span>)
-  //   );
-  // });
-
-  anime
-    .timeline({ loop: true })
-    .add({
-      targets: '.ml1 .letter',
-      scale: [0.3, 1],
-      opacity: [0, 1],
-      translateZ: 0,
-      easing: 'easeOutExpo',
-      duration: 600,
-      delay: function(el, i) {
-        return 70 * (i + 1);
-      }
-    })
-    .add({
-      targets: '.ml1 .line',
-      scaleX: [0, 1],
-      opacity: [0.5, 1],
-      easing: 'easeOutExpo',
-      duration: 700,
-      offset: '-=875',
-      delay: function(el, i, l) {
-        return 80 * (l - i);
-      }
-    })
-    .add({
-      targets: '.ml1',
-      opacity: 0,
-      duration: 1000,
-      easing: 'easeOutExpo',
-      delay: 1000
-    });
-
-  return (
-    <div className="container">
-      <div className="container-left">
-        <img src={WORKSPACE} alt="workspace" className="heading-img" />
-      </div>
-      <div className="container-right">
-        <h1>I am a</h1>
-        <h1 className="ml1">
-          <span className="text-wrapper">
-            <span className="line line1" />
-            <span className="letters">SOFTWARE DESIGNER</span>
-            <span className="line line2" />
-          </span>
-        </h1>
+const Heading = props => (
+  <div className="container">
+    <div className="container-left">
+      <img src={WORKSPACE} alt="workspace" className="heading-img" />
+    </div>
+    <div className="container-right">
+      <h3>My name is</h3>
+      <h1>Allison Kopp</h1>
+      <h3>and I am a</h3>
+      <h1>Software Developer</h1>
+      <div className="btn-container">
+        <button className="btn draw-border">About</button>
+        <button className="btn draw-border">Projects</button>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Heading;
